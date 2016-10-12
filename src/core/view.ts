e8b7b78059c138b039b38b11a722824df986de1c
Jonathan Hedrén <jonathan.hedren@gmail.com>
2016-10-12 06:48:39 +0200

    Remove automatic whitespace insertion

--- src/core/view.ts
+++ src/core/view.ts
@@ -1,6 +1,6 @@
 import { initComponent, updateComponent } from './component'
 import { typeOf, max } from './helpers'
-import { Dispatch, Task, UpdateAny, FieldValidator, ElementEventAttributeArguments, NodeDescriptor, TextNodeDescriptor, ElementNodeDescriptor, ComponentNodeDescriptor, ListenerWithEventOptions, FormAttributes, InputAttributes } from './contract'
+import { Dispatch, Task, UpdateAny, FieldValidator, ElementEventAttributeArguments, NodeDescriptor, ElementNodeDescriptor, ComponentNodeDescriptor, ListenerWithEventOptions, FormAttributes, InputAttributes } from './contract'
 import { validateField, validateForm } from './validation'
 
 type EventInterceptor = (nodeDescriptor: NodeDescriptor, element: Element, event: Event, update: UpdateAny, dispatch: Dispatch) => boolean
@@ -8,8 +8,8 @@ type EventInterceptor = (nodeDescriptor: NodeDescriptor, element: Element, event
 const eventInterceptors = [] as EventInterceptor[]
 
 const INPUT_TAG_NAMES = [
-    'INPUT', 
-    'TEXTAREA', 
+    'INPUT',
+    'TEXTAREA',
     'SELECT'
 ]
 
@@ -23,7 +23,7 @@ eventInterceptors.push((_nodeDescriptor, element, _, update, dispatch) => {
     return false
 })
 
-function findFieldValidatorsRec(nodeDescriptors: NodeDescriptor[], fields: { [name: string]: FieldValidator[]}) {
+function findFieldValidatorsRec(nodeDescriptors: NodeDescriptor[], fields: { [name: string]: FieldValidator[] }) {
     return nodeDescriptors.reduce((acc, descriptor) => {
         if (descriptor.__type === 'element') {
             const fieldName = (descriptor.attributes as InputAttributes).name
@@ -49,7 +49,7 @@ eventInterceptors.push((nodeDescriptor, element, _, update, dispatch) => {
             formData[el.name] = el.value
         }
         const formValidators = ((nodeDescriptor as ElementNodeDescriptor).attributes as FormAttributes).validate || []
-        
+
         const fieldValidators = findFieldValidatorsRec((nodeDescriptor as ElementNodeDescriptor).children, {})
         const validationResult = validateForm(formData, Array.isArray(formValidators) ? formValidators : [formValidators], fieldValidators)
         dispatch(update, formData, validationResult)
@@ -85,7 +85,7 @@ const KEY_MAP = {
 } as { [key: string]: number }
 
 function nodesEqual(a: Node | undefined, b: Node) {
-    return typeof a !== 'undefined' && (a === b || a.nodeType === Node.COMMENT_NODE && b.nodeType === Node.COMMENT_NODE && a.nodeValue === b.nodeValue) 
+    return typeof a !== 'undefined' && (a === b || a.nodeType === Node.COMMENT_NODE && b.nodeType === Node.COMMENT_NODE && a.nodeValue === b.nodeValue)
 }
 
 /** Sets an attribute or event listener on an HTMLElement. */
@@ -96,13 +96,13 @@ function setAttr(element: HTMLElement, attributeName: string, attributeValue: an
             // If there is a previous event listener, wrap it and the new one in
             // a function calling both.
             const prevListener = (element as any)[eventName]
-            ;(element as any)[eventName] = (ev: Event) => {
-                prevListener(ev)
-                attributeValue(ev)
-            }
+                ; (element as any)[eventName] = (ev: Event) => {
+                    prevListener(ev)
+                    attributeValue(ev)
+                }
         }
         else {
-            ;(element as any)[eventName] = attributeValue
+            ; (element as any)[eventName] = attributeValue
         }
     }
     else if (attributeName === 'value' && INPUT_TAG_NAMES.indexOf(element.tagName) >= 0) {
@@ -121,7 +121,7 @@ function setAttr(element: HTMLElement, attributeName: string, attributeValue: an
 
 /** Removes an attribute or event listener from an HTMLElement. */
 function removeAttr(a: string, node: Element) {
-    const [eventName, ] = a.substr(2).toLowerCase().split('_')
+    const [eventName,] = a.substr(2).toLowerCase().split('_')
     if (a.indexOf('on') === 0) {
         (node as any)['on' + eventName.toLowerCase()] = null
     }
@@ -148,7 +148,7 @@ function tryCreateEventListener(attributeName: string, eventArgs: ElementEventAt
             throw `Unhandled key '${key}'. Use a key code instead, for example keyup_65 for 'a'.`
         }
     }
-    
+
     return (ev: Event) => {
 
         if (isKeyboardEvent) {
@@ -163,7 +163,7 @@ function tryCreateEventListener(attributeName: string, eventArgs: ElementEventAt
         if ((eventArgs as Task).execute) {
             (eventArgs as Task).execute((eventArgs as any).__dispatch || dispatch)
             return
-        } 
+        }
         else if (eventArgsType === 'object') {
             if ((eventArgs as ListenerWithEventOptions).preventDefault) {
                 ev.preventDefault()
@@ -181,10 +181,10 @@ function tryCreateEventListener(attributeName: string, eventArgs: ElementEventAt
                 }
             }
         }
-        
-        const intercepted = eventInterceptors.reduce((stop, interceptor) => 
+
+        const intercepted = eventInterceptors.reduce((stop, interceptor) =>
             stop ? stop : interceptor(nodeDescriptor, node as Element, ev, eventArgs as UpdateAny, (eventArgs as any).__dispatch || dispatch), false)
-        
+
         if (!intercepted) {
             if ((eventArgs as any).__dispatch) {
                 (eventArgs as any).__dispatch(eventArgs as UpdateAny)
@@ -199,8 +199,8 @@ function tryCreateEventListener(attributeName: string, eventArgs: ElementEventAt
 
 /** Creates a Node from a NodeDescriptor. */
 function createNode(descriptor: NodeDescriptor, parentNode: Element, dispatch: Dispatch): Node {
-    switch(descriptor.__type) {
-        case 'element':  
+    switch (descriptor.__type) {
+        case 'element':
             return document.createElement(descriptor.tagName)
         case 'text':
             return document.createTextNode(descriptor.value)
@@ -232,20 +232,20 @@ function shouldReplaceNode(newDescriptor: NodeDescriptor, oldDescriptor: NodeDes
 
 function getAttributesToRemove(newDescriptor: ElementNodeDescriptor, oldDescriptor: NodeDescriptor, existingNode: Node) {
     const oldAttributeKeys = Object.keys((oldDescriptor as ElementNodeDescriptor).attributes)
-    
+
     if (existingNode !== oldDescriptor.node) {
         return oldAttributeKeys
     }
-    
+
     const newAttributeKeys = Object.keys(newDescriptor.attributes)
     return newAttributeKeys.filter(x => oldAttributeKeys.indexOf(x) === -1)
-                           .concat(oldAttributeKeys.filter(x => newAttributeKeys.indexOf(x) === -1 || x.indexOf('on') === 0))
+        .concat(oldAttributeKeys.filter(x => newAttributeKeys.indexOf(x) === -1 || x.indexOf('on') === 0))
 }
 
 /** Renders the view by walking the node descriptor tree recursively */
 export function render(parentNode: Element, newDescriptor: NodeDescriptor, oldDescriptor: NodeDescriptor, existingNode: Node | undefined, dispatch: Dispatch): void {
     const replaceNode = shouldReplaceNode(newDescriptor, oldDescriptor)
-    if (typeof existingNode === 'undefined' || existingNode === null || replaceNode) {
+    if (!existingNode || !oldDescriptor || replaceNode) {
         // if no existing node, create one
         const newNode = createNode(newDescriptor, parentNode, dispatch)
 
@@ -254,9 +254,9 @@ export function render(parentNode: Element, newDescriptor: NodeDescriptor, oldDe
         if (replaceNode) {
             if (oldDescriptor.__type === 'element') {
                 // Remove old event listeners before replacing the node. 
-                Object.keys(oldDescriptor.attributes).filter(a => a.indexOf('on') === 0).forEach(a => {
+                Object.keys(oldDescriptor.attributes).filter(a => a.indexOf('on') === 0).forEach(a => 
                     removeAttr(a, existingNode as Element)
-                })
+                )
             }
 
             parentNode.replaceChild(newNode, existingNode!)
@@ -278,12 +278,8 @@ export function render(parentNode: Element, newDescriptor: NodeDescriptor, oldDe
             }
 
             newDescriptor.children
-                        .filter(c => typeof c !== 'undefined')
-                        .forEach(c => {
-                            render(newNode as Element, c, c, undefined, dispatch)
-                            // add "whitespace" between elements
-                            newNode.appendChild(document.createTextNode('\r\n'))
-                        })
+                .filter(c => typeof c !== 'undefined')
+                .forEach(c => render(newNode as Element, c, c, undefined, dispatch))
         }
     }
     else { // reuse the old node
@@ -300,18 +296,18 @@ export function render(parentNode: Element, newDescriptor: NodeDescriptor, oldDe
         // update existing node
         switch (newDescriptor.__type) {
             case 'element':
-            
+
                 // remove any attributes that was added with the old node descriptor but does not exist in the new descriptor.
                 getAttributesToRemove(newDescriptor, oldDescriptor, existingNode).forEach(a => {
                     removeAttr(a, existingNode as Element)
                 })
-                
+
                 // update any attribute where the attribute value has changed
                 for (const name in newDescriptor.attributes) {
                     if (newDescriptor.attributes.hasOwnProperty(name)) {
                         const attributeValue = newDescriptor.attributes[name]
                         const oldAttributeValue = (oldDescriptor as ElementNodeDescriptor).attributes[name]
-                        if ((name.indexOf('on') === 0 || !(existingNode as Element).hasAttribute(name) || 
+                        if ((name.indexOf('on') === 0 || !(existingNode as Element).hasAttribute(name) ||
                             attributeValue !== oldAttributeValue) && typeof attributeValue !== 'undefined'
                         ) {
                             const eventListener = tryCreateEventListener(name, attributeValue, newDescriptor, existingNode, dispatch)
@@ -329,45 +325,20 @@ export function render(parentNode: Element, newDescriptor: NodeDescriptor, oldDe
                 const maxIterations = max(newDescriptorChildLengh, oldDescriptorChildrenLength)
 
                 let childDescriptorIndex = 0
-                let childNodeIndex = 0
-                while (true) {
-                    if (childDescriptorIndex >= maxIterations) {
-                        break
-                    }
+                for (let i = 0; i < maxIterations; i++) {
 
-                    const childNode = existingNode!.childNodes.item(childNodeIndex)
-                    const childNodeIsCLRF = !!childNode && childNode.nodeType === Node.TEXT_NODE && childNode.textContent === '\r\n'
+                    const childNode = existingNode!.childNodes.item(childDescriptorIndex)
 
-                    if (childNodeIsCLRF) {
-                        // This is a check for an edge case where a text node with value '\r\n' 
-                        // has been added in a view structure.
-                        const oldDescriptorChildIsCLRF = 
-                            oldDescriptor.__type === 'element' && 
-                            !!oldDescriptor!.children[childDescriptorIndex] &&
-                            oldDescriptor!.children[childDescriptorIndex].__type === 'text' && 
-                            (oldDescriptor!.children[childDescriptorIndex] as TextNodeDescriptor).value === '\r\n'
-                        
-                        if (!oldDescriptorChildIsCLRF) {
-                            childNodeIndex++
-                        }
-                        continue
-                    }
-                    else if (childDescriptorIndex < newDescriptorChildLengh) {
-                        const childDescriptor = newDescriptor.children[childDescriptorIndex]
-                        
-                        render(existingNode as Element, childDescriptor, oldDescriptor.__type === 'element' ? oldDescriptor!.children[childDescriptorIndex] : childDescriptor, childNode, dispatch)
-                        
-                        if (!childNode) {
-                            // add "whitespace" between elements
-                            existingNode.appendChild(document.createTextNode('\r\n'))
-                        }
+                    if (i < newDescriptorChildLengh) {
+                        const childDescriptor = newDescriptor.children[i]
+
+                        render(existingNode as Element, childDescriptor, oldDescriptor.__type === 'element' ? oldDescriptor!.children[i] : childDescriptor, childNode, dispatch)
+
+                        childDescriptorIndex++
                     }
                     else if (childNode) {
                         existingNode.removeChild(childNode)
-                    }          
-
-                    childDescriptorIndex++
-                    childNodeIndex++
+                    }
                 }
                 break
             case 'text':
