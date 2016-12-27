2b847ed75fc54d0e2e7507e1737065da577d5d79
Jonathan Hedrén <jonathan.hedren@gmail.com>
2016-12-27 09:21:51 +0100

    Refactoring, add comments

--- src/view.ts
+++ src/view.ts
@@ -46,82 +46,10 @@ export function render(
     }
 }
 
-/** Sets an attribute or event listener on an HTMLElement. */
-function setAttr(element: HTMLElement, attributeName: string, attributeValue: any) {
-    if (attributeName.indexOf('on') === 0) {
-        if (typeof (element as any)[attributeName] === 'function') {
-            // If there is a previous event listener, wrap it and the new one in
-            // a function calling both.
-            const prevListener = (element as any)[attributeName]
-                ; (element as any)[attributeName] = (ev: Event) => {
-                    prevListener(ev)
-                    attributeValue(ev)
-                }
-        }
-        else {
-            ; (element as any)[attributeName] = attributeValue
-        }
-    }
-    else if (attributeName === 'value' && INPUT_TAG_NAMES.indexOf(element.tagName) >= 0) {
-        (element as HTMLInputElement).value = attributeValue
-    }
-    else if (BOOL_ATTRS.indexOf(attributeName) >= 0) {
-        (element as any)[attributeName] = !!attributeValue
-    }
-    else if (attributeValue && CALLABLE_ATTRS.indexOf(attributeName) >= 0) {
-        (element as any)[attributeName]()
-    }
-    else if (typeof attributeValue !== 'function' && typeof attributeValue !== 'object') {
-        element.setAttribute(attributeName, attributeValue)
-    }
-}
-
-/** Removes an attribute or event listener from an HTMLElement. */
-function removeAttr(a: string, node: Element) {
-    if (a.indexOf('on') === 0) {
-        (node as any)[a] = null
-    }
-    else if (node.hasAttribute(a)) {
-        node.removeAttribute(a)
-    }
-}
-
-/** Creates an event listener */
-function tryCreateEventListener(attributeName: string, eventListener: EventListener<any, any>, nodeDescriptor: ElementDescriptor<any>) {
-    if (attributeName.indexOf('on') !== 0) {
-        return undefined
-    }
-
-    return (ev: Event) => {
-        eventListener(ev, ev.target, nodeDescriptor)
-    }
-}
-
-/** Creates a Node from a NodeDescriptor. */
-function createNode(descriptor: NodeDescriptor, parentNode: Element): Node {
-    switch (descriptor.__type) {
-        case 2:
-            return document.createElement(descriptor.tagName)
-        case 1:
-            return document.createTextNode(descriptor.value)
-        case 3:
-            initComponent(descriptor, parentNode)
-            return descriptor.node!
-        case 0:
-            return document.createComment('Nothing')
-    }
-}
-
-function getAttributesToRemove(newDescriptor: ElementDescriptor<any>, oldDescriptor: NodeDescriptor) {
-    const attributesToRemove = [] as string[]
-    for (const attributeName in (oldDescriptor as ElementDescriptor<any>).attributes) {
-        if (typeof (newDescriptor.attributes as any)[attributeName] === 'undefined' || attributeName.indexOf('on') === 0) {
-            attributesToRemove.push(attributeName)
-        }
-    }
-    return attributesToRemove
-}
-
+/** 
+ * Renders a new descriptor by creating a new DOM node and attaching it to it's 
+ * parent.
+ */
 function renderNewNode(replaceNode: boolean, parentNode: Element, newDescriptor: NodeDescriptor, oldDescriptor: NodeDescriptor, existingNode: Node | undefined) {
     // if no existing node, create one
     const newNode = createNode(newDescriptor, parentNode)
@@ -167,128 +95,7 @@ function renderNewNode(replaceNode: boolean, parentNode: Element, newDescriptor:
     }
 }
 
-function updateElementAttributes(newDescriptor: ElementDescriptor<any>, oldDescriptor: NodeDescriptor, existingNode: Node) {
-    // remove any attributes that was added with the old node descriptor but does not exist in the new descriptor.
-    for (const attr of getAttributesToRemove(newDescriptor, oldDescriptor)) {
-        removeAttr(attr, existingNode as Element)
-    }
-
-    let attributeValue: any
-    let oldAttributeValue: any
-    let eventListener: EventListener<any, any> | undefined
-
-    // update any attribute where the attribute value has changed
-    for (const name in newDescriptor.attributes) {
-        if (newDescriptor.attributes.hasOwnProperty(name)) {
-            attributeValue = (newDescriptor.attributes as any)[name]
-            oldAttributeValue = ((oldDescriptor as ElementDescriptor<any>).attributes as any)[name]
-            if ((name.indexOf('on') === 0 || attributeValue !== oldAttributeValue ||
-                !(existingNode as Element).hasAttribute(name)) && typeof attributeValue !== 'undefined'
-            ) {
-                eventListener = tryCreateEventListener(name, attributeValue, newDescriptor)
-                setAttr(
-                    existingNode as HTMLElement,
-                    name,
-                    typeof eventListener === 'undefined' ? attributeValue : eventListener
-                )
-            }
-            else if (typeof attributeValue === 'undefined' && (existingNode as Element).hasAttribute(name)) {
-                (existingNode as Element).removeAttribute(name)
-            }
-        }
-    }
-}
-
-function findOldChildDescriptor(childDescriptor: NodeDescriptor, oldDescriptor: NodeDescriptor, childIndex: number) {
-    if (oldDescriptor.__type === 2 || oldDescriptor.__type === 3) {
-        const oldChildDescriptor = oldDescriptor.children[childIndex]
-        if (typeof childDescriptor !== 'undefined' && typeof oldChildDescriptor !== 'undefined') {
-            if (childDescriptor.__type === 2 && oldChildDescriptor.__type === 2
-                && childDescriptor.attributes.key !== oldChildDescriptor.attributes.key) {
-                let child: ElementDescriptor<any>
-                for (let i = 0; i < oldDescriptor.children.length; i++) {
-                    child = oldDescriptor.children[i] as ElementDescriptor<any>
-                    if (child.__type === 2 && child.attributes.key === childDescriptor.attributes.key) {
-                        return child
-                    }
-                }
-            }
-            else if (childDescriptor.__type === 3 && oldChildDescriptor.__type === 3
-                && childDescriptor.props.key !== oldChildDescriptor.props.key) {
-                let child: ComponentDescriptor<any>
-                for (let i = 0; i < oldDescriptor.children.length; i++) {
-                    child = oldDescriptor.children[i] as ComponentDescriptor<any>
-                    if (child.__type === 3 && child.props.key === childDescriptor.props.key) {
-                        return child
-                    }
-                }
-            }
-        }
-        else if (typeof oldChildDescriptor === 'undefined') {
-            return childDescriptor
-        }
-
-        return oldChildDescriptor
-    }
-    return childDescriptor
-}
-
-
-function renderChildNodes(newDescriptor: ElementDescriptor<any>, oldDescriptor: NodeDescriptor, existingNode: Node) {
-
-    // Iterate over children and add/update/remove nodes
-    const newDescriptorChildLengh = newDescriptor.children.length
-    const oldDescriptorChildrenLength = oldDescriptor.__type === 2 ? oldDescriptor.children.length : 0
-    const maxIterations = max(newDescriptorChildLengh, oldDescriptorChildrenLength)
-    let childDescriptorIndex = 0
-    let childNode: Node | null = existingNode.firstChild
-    let childDescriptor: NodeDescriptor
-
-    for (let i = 0; i < maxIterations; i++) {
-        if (i < newDescriptorChildLengh) {
-            childDescriptor = newDescriptor.children[i]
-
-            const oldChildDescriptor = findOldChildDescriptor(childDescriptor, oldDescriptor, i)
-            if (typeof oldChildDescriptor.node !== 'undefined' && oldChildDescriptor.node !== childNode) {
-                existingNode.insertBefore(oldChildDescriptor.node, childNode)
-            }
-            else if (childNode !== null) {
-                childNode = childNode!.nextSibling
-            }
-
-            render(existingNode as Element, childDescriptor, oldChildDescriptor, oldChildDescriptor.node)
-            childDescriptorIndex++
-        }
-        else if (childNode !== null) {
-            const oldChildDescriptor = (oldDescriptor as ElementDescriptor<any>).children[i]
-            findAndUnmountComponentsRec(oldChildDescriptor)
-            const nextSibling = childNode.nextSibling
-            existingNode.removeChild(childNode)
-            childNode = nextSibling
-        }
-
-    }
-}
-
-/** Returns true if the node should be replaced, given the new and old descriptors. */
-function shouldReplaceNode(newDescriptor: NodeDescriptor, oldDescriptor: NodeDescriptor | undefined): boolean {
-    if (typeof oldDescriptor === 'undefined') {
-        return false
-    }
-    else if (newDescriptor.__type !== oldDescriptor.__type) {
-        return true
-    }
-    else if (newDescriptor.__type === 2 && oldDescriptor.__type === 2 &&
-        newDescriptor.tagName !== oldDescriptor.tagName) {
-        return true
-    }
-    else if (newDescriptor.__type === 3 && oldDescriptor.__type === 3 &&
-        newDescriptor.name !== oldDescriptor.name) {
-        return true
-    }
-    return false
-}
-
+/** Re-renders a descriptor by reusing a DOM node. */
 function reRenderNode(newDescriptor: NodeDescriptor, oldDescriptor: NodeDescriptor, existingNode: Node) {
 
     // if (!nodesEqual(oldDescriptor.node, existingNode)) {
@@ -322,5 +129,229 @@ function reRenderNode(newDescriptor: NodeDescriptor, oldDescriptor: NodeDescript
     if (newDescriptor !== oldDescriptor) {
         // clean up
         oldDescriptor.node = undefined
+    }
+}
+
+/** Renders child descriptors. Will add/remove DOM nodes if needed. */
+function renderChildNodes(newDescriptor: ElementDescriptor<any>, oldDescriptor: NodeDescriptor, existingNode: Node) {
+
+    // Iterate over children and add/update/remove nodes
+    const newDescriptorChildLengh = newDescriptor.children.length
+    const oldDescriptorChildrenLength = oldDescriptor.__type === 2 ? oldDescriptor.children.length : 0
+    const maxIterations = max(newDescriptorChildLengh, oldDescriptorChildrenLength)
+
+    let childDescriptorIndex = 0
+    let childNode: Node | null = existingNode.firstChild
+    let childDescriptor: NodeDescriptor
+
+    for (let i = 0; i < maxIterations; i++) {
+
+        if (i < newDescriptorChildLengh) {
+
+            childDescriptor = newDescriptor.children[i]
+
+            const oldChildDescriptor = findOldChildDescriptor(childDescriptor, oldDescriptor, i)
+            if (typeof oldChildDescriptor.node !== 'undefined' && oldChildDescriptor.node !== childNode) {
+                existingNode.insertBefore(oldChildDescriptor.node, childNode)
+            }
+            else if (childNode !== null) {
+                childNode = childNode!.nextSibling
+            }
+
+            render(existingNode as Element, childDescriptor, oldChildDescriptor, oldChildDescriptor.node)
+
+            childDescriptorIndex++
+        }
+        else if (childNode !== null) {
+            const oldChildDescriptor = (oldDescriptor as ElementDescriptor<any>).children[i]
+
+            // Make sure that any components are unmounted correctly
+            findAndUnmountComponentsRec(oldChildDescriptor)
+
+            // Get a reference to the next sibling before the child node is
+            // removed.
+            const nextSibling = childNode.nextSibling
+            existingNode.removeChild(childNode)
+            childNode = nextSibling
+        }
+
+    }
+}
+
+/** 
+ * Tries to find an old "keyed" descriptor that matches the new descriptor. 
+ */
+function findOldChildDescriptor(childDescriptor: NodeDescriptor, oldDescriptor: NodeDescriptor, childIndex: number) {
+    if (oldDescriptor.__type === 2 || oldDescriptor.__type === 3) {
+
+        const oldChildDescriptor = oldDescriptor.children[childIndex]
+
+        if (typeof childDescriptor !== 'undefined' && typeof oldChildDescriptor !== 'undefined') {
+
+            // If keyed ElementDescriptors...
+            if (childDescriptor.__type === 2 && oldChildDescriptor.__type === 2
+                && childDescriptor.attributes.key !== oldChildDescriptor.attributes.key) {
+
+                let child: ElementDescriptor<any>
+
+                for (let i = 0; i < oldDescriptor.children.length; i++) {
+
+                    child = oldDescriptor.children[i] as ElementDescriptor<any>
+
+                    if (child.__type === 2 && child.attributes.key === childDescriptor.attributes.key) {
+                        return child
+                    }
+                }
+            }
+            // If keyed ComponentDescriptors...
+            else if (childDescriptor.__type === 3 && oldChildDescriptor.__type === 3
+                && childDescriptor.props.key !== oldChildDescriptor.props.key) {
+
+                let child: ComponentDescriptor<any>
+
+                for (let i = 0; i < oldDescriptor.children.length; i++) {
+
+                    child = oldDescriptor.children[i] as ComponentDescriptor<any>
+
+                    if (child.__type === 3 && child.props.key === childDescriptor.props.key) {
+                        return child
+                    }
+                }
+            }
+        }
+        else if (typeof oldChildDescriptor === 'undefined') {
+            return childDescriptor
+        }
+
+        return oldChildDescriptor
+    }
+    return childDescriptor
+}
+
+
+/** Returns true if the node should be replaced, given the new and old descriptors. */
+function shouldReplaceNode(newDescriptor: NodeDescriptor, oldDescriptor: NodeDescriptor | undefined): boolean {
+    if (typeof oldDescriptor === 'undefined') {
+        return false
+    }
+    else if (newDescriptor.__type !== oldDescriptor.__type) {
+        return true
+    }
+    else if (newDescriptor.__type === 2 && oldDescriptor.__type === 2 &&
+        newDescriptor.tagName !== oldDescriptor.tagName) {
+        return true
+    }
+    else if (newDescriptor.__type === 3 && oldDescriptor.__type === 3 &&
+        newDescriptor.name !== oldDescriptor.name) {
+        return true
+    }
+    return false
+}
+
+/** Sets an attribute or event listener on an HTMLElement. */
+function setAttr(element: HTMLElement, attributeName: string, attributeValue: any) {
+    if (attributeName.indexOf('on') === 0) {
+        if (typeof (element as any)[attributeName] === 'function') {
+            // If there is a previous event listener, wrap it and the new one in
+            // a function calling both.
+            const prevListener = (element as any)[attributeName]
+                ; (element as any)[attributeName] = (ev: Event) => {
+                    prevListener(ev)
+                    attributeValue(ev)
+                }
+        }
+        else {
+            ; (element as any)[attributeName] = attributeValue
+        }
+    }
+    else if (attributeName === 'value' && INPUT_TAG_NAMES.indexOf(element.tagName) >= 0) {
+        (element as HTMLInputElement).value = attributeValue
+    }
+    else if (BOOL_ATTRS.indexOf(attributeName) >= 0) {
+        (element as any)[attributeName] = !!attributeValue
+    }
+    else if (attributeValue && CALLABLE_ATTRS.indexOf(attributeName) >= 0) {
+        (element as any)[attributeName]()
+    }
+    else if (typeof attributeValue !== 'function' && typeof attributeValue !== 'object') {
+        element.setAttribute(attributeName, attributeValue)
+    }
+}
+
+/** Removes an attribute or event listener from an HTMLElement. */
+function removeAttr(a: string, node: Element) {
+    if (a.indexOf('on') === 0) {
+        (node as any)[a] = null
+    }
+    else if (node.hasAttribute(a)) {
+        node.removeAttribute(a)
+    }
+}
+
+/** Creates an event listener if the attribute name begins with 'on'. */
+function tryCreateEventListener(attributeName: string, eventListener: EventListener<any, any>, nodeDescriptor: ElementDescriptor<any>) {
+    if (attributeName.indexOf('on') !== 0) {
+        return undefined
+    }
+
+    return (ev: Event) => {
+        eventListener(ev, ev.target, nodeDescriptor)
+    }
+}
+
+/** Creates a Node from a NodeDescriptor. */
+function createNode(descriptor: NodeDescriptor, parentNode: Element): Node {
+    switch (descriptor.__type) {
+        case 2:
+            return document.createElement(descriptor.tagName)
+        case 1:
+            return document.createTextNode(descriptor.value)
+        case 3:
+            initComponent(descriptor, parentNode)
+            return descriptor.node!
+        case 0:
+            return document.createComment('Nothing')
+    }
+}
+
+function getAttributesToRemove(newDescriptor: ElementDescriptor<any>, oldDescriptor: NodeDescriptor) {
+    const attributesToRemove = [] as string[]
+    for (const attributeName in (oldDescriptor as ElementDescriptor<any>).attributes) {
+        if (typeof (newDescriptor.attributes as any)[attributeName] === 'undefined' || attributeName.indexOf('on') === 0) {
+            attributesToRemove.push(attributeName)
+        }
+    }
+    return attributesToRemove
+}
+
+function updateElementAttributes(newDescriptor: ElementDescriptor<any>, oldDescriptor: NodeDescriptor, existingNode: Node) {
+    // remove any attributes that was added with the old node descriptor but does not exist in the new descriptor.
+    for (const attr of getAttributesToRemove(newDescriptor, oldDescriptor)) {
+        removeAttr(attr, existingNode as Element)
+    }
+
+    let attributeValue: any
+    let oldAttributeValue: any
+    let eventListener: EventListener<any, any> | undefined
+
+    // update any attribute where the attribute value has changed
+    for (const name in newDescriptor.attributes) {
+        if (newDescriptor.attributes.hasOwnProperty(name)) {
+            attributeValue = (newDescriptor.attributes as any)[name]
+            oldAttributeValue = ((oldDescriptor as ElementDescriptor<any>).attributes as any)[name]
+            if ((name.indexOf('on') === 0 || attributeValue !== oldAttributeValue ||
+                !(existingNode as Element).hasAttribute(name)) && typeof attributeValue !== 'undefined'
+            ) {
+                eventListener = tryCreateEventListener(name, attributeValue, newDescriptor)
+                setAttr(
+                    existingNode as HTMLElement,
+                    name,
+                    typeof eventListener === 'undefined' ? attributeValue : eventListener
+                )
+            }
+            else if (typeof attributeValue === 'undefined' && (existingNode as Element).hasAttribute(name)) {
+                (existingNode as Element).removeAttribute(name)
+            }
+        }
     }
 }
