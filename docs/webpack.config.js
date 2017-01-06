d7e739a826c11059568d874732edea6a8c6f7e62
Jonathan Hedrén <jonathan.hedren@gmail.com>
2017-01-06 14:13:21 +0100

    Change index page for webpack dev server

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -20,7 +20,9 @@ module.exports = {
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
     devServer: {
-        historyApiFallback: true
+        historyApiFallback: {
+            index: '404.html'
+        }
     },
     module: {
         rules: [
