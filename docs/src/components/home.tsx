ac09f87405e7c590896afddc9ca4f19d2a0df324
Jonathan Hedrén <jonathan.hedren@gmail.com>
2017-01-12 22:16:53 +0100

    Update docs homepage

--- docs/src/components/home.tsx
+++ docs/src/components/home.tsx
@@ -6,33 +6,38 @@ export default () =>
     <div id="home">
         <div class="container">
             <h1>Myra</h1>
-            <p class="lead">A simple and small Typescript framework for building web interfaces</p>
+            <p class="lead">
+                A simple and small Typescript framework for building web interfaces
+            </p>
             <Navigation />
-            <ul>
-                <li>
-                    <strong>Functional:</strong>
-                    Myra encourages functional programming and immutability for predictable
-                    behavior.
-                </li>
-                <li>
-                    <strong>Small API:</strong>
-                    Myra should be easy to learn as it's API and concepts are limited.
-                </li>
-                <li>
-                    <strong>Statically typed views:</strong>
-                    Myra does not use HTML templates but uses
-                    <a href="https://facebook.github.io/react/docs/jsx-in-depth.html">JSX</a>
-                    to build up view hierarchies. With Typescript's type checking
-                    errors can catched in the build step.
-                </li>
-                <li>
-                    <strong>No dependencies:</strong>
-                    Myra does not depend on any external libraries.
-                </li>
-                <li>
-                    <strong>Small code base/size:</strong>
-                    Hello World in ~4kb minified and gzipped
-                </li>
-            </ul>
+
+        </div>
+        <div id="features">
+            <div class="container">
+                <h2>Features</h2>
+                <ul>
+                    <li>
+                        <h3>Simple</h3>
+                        <p>
+                            Myra should be easy to learn as it's API and 
+                            concepts are limited.
+                        </p>
+                    </li>
+                    <li>
+                        <h3>Small</h3>
+                        <p>
+                            Hello World in ~4kb minified and gzipped.
+                            Also, no external dependencies.
+                        </p>
+                    </li>
+                    <li>
+                        <h3>Functional</h3>
+                        <p>
+                            Myra encourages functional programming and 
+                            immutability for predictable behavior.
+                        </p>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
