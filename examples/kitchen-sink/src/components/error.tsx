cdb894f5bf008e3ea0fdd8dae9ea55e1b2eb8be9
Jonathan Hedrén <jonathan.hedren@gmail.com>
2018-12-30 22:02:34 +0100

    Add error handling example

---
+++ examples/kitchen-sink/src/components/error.tsx
@@ -0,0 +1,26 @@
+import * as myra from 'myra'
+
+function throwError(from: string) {
+    throw Error(from)
+}
+
+export const ErrorComponent = myra.define({}, ctx => {
+
+    ctx.onError = err =>
+        <div>
+            <h2>Oops! An error occured</h2>
+            <p>{err}</p>
+        </div>
+
+    // Uncomment any of the following lines to test error handling in different
+    // contexts
+    //throwError('initialization')
+    //ctx.didMount = () => throwError('didMount')
+    //ctx.didRender = () => throwError('didRender')
+    //ctx.shouldRender = () => throwError('shouldRender')
+    //ctx.willMount = () => throwError('willMount')
+    //ctx.willRender = () => throwError('willRender')
+
+    return () =>
+        <div>This will never be displayed because {throwError('view')}</div>
+})
--- examples/kitchen-sink/src/components/main.tsx
+++ examples/kitchen-sink/src/components/main.tsx
@@ -4,20 +4,26 @@ import StatelessComponent from './stateless-component'
 import CounterComponent from './counter'
 import HttpComponent from './http'
 import TimeComponent from './time'
+import { ErrorComponent } from './error'
 
 
 /**
  * View
  */
-export const MainComponent = myra.define({}, () => () =>
-    <div class="container">
-        <h1>Kitchen sink demo</h1>
-        <hr />
-        <StatelessComponent test="a value" />
-        <hr />
-        <CounterComponent />
-        <hr />
-        <HttpComponent />
-        <hr />
-        <TimeComponent />
-    </div>)
+export const MainComponent = myra.define({}, _ => {
+
+    return () =>
+        <div class="container">
+            <h1>Kitchen sink demo</h1>
+            <hr />
+            <StatelessComponent test="a value" />
+            <hr />
+            <CounterComponent />
+            <hr />
+            <HttpComponent />
+            <hr />
+            <ErrorComponent />
+            <hr />
+            <TimeComponent />
+        </div>
+})
