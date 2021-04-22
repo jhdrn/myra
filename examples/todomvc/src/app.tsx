8987f308d814a2a0b97e7025dad4d8be9c0ad111
Jonathan Hedrén <jonathan.hedren@gmail.com>
2021-04-22 15:28:37 +0200

    Remove old examples and lab stuff

--- examples/todomvc/src/app.tsx
+++
@@ -1,4 +0,0 @@
-import * as myra from 'myra'
-import { MainComponent } from './components/main'
-
-myra.mount(<MainComponent />, document.body)
--- lab/kitchen-sink/src/app.tsx
+++
@@ -1,4 +0,0 @@
-import * as myra from '../../../src/myra'
-import { MainComponent } from './components/main'
-
-myra.mount(<MainComponent />, document.body)
