5823631aa7f74286183014aa31c065ec46b4b4e5
Jonathan Hedrén <jonathan.hedren@gmail.com>
2021-05-03 08:58:53 +0200

    Add some fragment tests

--- test/fragments.spec.tsx
+++ test/fragments.spec.tsx
@@ -6,14 +6,68 @@ describe('fragment', () => {
 
     it('renders fragment content', done => {
 
-        const Component = () => <><div id="root" /></>
+        const Component = () => <><div id="node" /></>
 
         myra.mount(<Component />, document.body)
 
         requestAnimationFrame(() => {
-            const rootNode = q('body > #root')
+            const node = q('body > #node')
 
-            expect(rootNode).not.toBeNull()
+            expect(node).not.toBeNull()
+
+            done()
+        })
+    })
+
+    it('renders nested fragment content', done => {
+
+        const Component = () => <div><><div id="node" /></></div>
+
+        myra.mount(<Component />, document.body)
+
+        requestAnimationFrame(() => {
+            const node = q('body > div > #node')
+
+            expect(node).not.toBeNull()
+
+            done()
+        })
+    })
+
+
+    it('renders multiple fragment child nodes', done => {
+
+        const Component = () => <><div id="node1" /><div id="node2" /></>
+
+        myra.mount(<Component />, document.body)
+
+        requestAnimationFrame(() => {
+            const node1 = q('body > #node1')
+            const node2 = q('body > #node2')
+
+            expect(node1).not.toBeNull()
+            expect(node2).not.toBeNull()
+
+            done()
+        })
+    })
+
+    it('renders special fragment child nodes', done => {
+
+        const ChildComponent = () => <div id="child"></div>
+        const Component = () => <><nothing /><ChildComponent /></>
+
+        const fragmentContainer = document.createElement('div')
+        document.body.appendChild(fragmentContainer)
+
+        myra.mount(<Component />, fragmentContainer)
+
+        requestAnimationFrame(() => {
+            const nothingNode = fragmentContainer.firstChild
+            const childNode = fragmentContainer.firstElementChild
+            expect(nothingNode).not.toBeNull()
+            expect(nothingNode?.nodeType).toBe(Node.COMMENT_NODE)
+            expect(childNode).not.toBeNull()
 
             done()
         })
